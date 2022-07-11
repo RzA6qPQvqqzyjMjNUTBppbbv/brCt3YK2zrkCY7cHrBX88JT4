d5cd396f879005bc1a3d2ad0febab78663952886
RzA6qPQvqqzyjMjNUTBppbbv <97688473+RzA6qPQvqqzyjMjNUTBppbbv@users.noreply.github.com>
2022-07-11 17:20:41 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+function main(){
+    if(!localStorage.getItem("theme")){
+        document.body.style.backgroundColor = "#FFF"
+        document.getElementById("theme").style.color = "#FFF";
+        document.getElementById("title").style.color = "rgb(46, 46, 49)"
+        return
+    }
+    if(localStorage.getItem("theme") == "black"){
+        document.body.style.backgroundColor = "rgb(46, 46, 49)";
+        document.getElementById("title").style.color = "#FFF";
+        document.getElementById("theme").style.color = "rgb(46, 46, 49)";
+        return
+    }
+}
 function theme(){
     if(document.body.style.backgroundColor == "rgb(255, 255, 255)"){  
+        localStorage.setItem("theme", "black");
         document.body.style.backgroundColor = "rgb(46, 46, 49)";
         document.getElementById("title").style.color = "#FFF";
         document.getElementById("theme").style.color = "rgb(46, 46, 49)";
     } else {
+        localStorage.setItem("theme", "white")
        document.body.style.backgroundColor = "#FFF"
        document.getElementById("theme").style.color = "#FFF";
        document.getElementById("title").style.color = "rgb(46, 46, 49)"
